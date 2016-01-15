83bc883b88a37a9ba1f6c7ec88595500d0685b8a
Andrej Antonov <polymorphm@gmail.com>
2016-01-16 00:43:52 +0300

    created 'setup.py'

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='lib_socks_proxy_2013_10_03',
+  version='master',
+  maintainer='Andrej Antonov',
+  maintainer_email='polymorphm@gmail.com',
+  url='https://github.com/polymorphm/lib-socks-proxy',
+  description='Python (Python-3.X) library for connection via SOCKS5-proxy',
+  license='MIT',
+  packages=[
+    'lib_socks_proxy_2013_10_03',
+    'lib_socks_proxy_2013_10_03.requests_support',
+  ],
+)
